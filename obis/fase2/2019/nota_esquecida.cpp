84d2c87257f896bc0c0e56daaad905e65e2aac74
gu-nog <gumendes.nog.dev@gmail.com>
2022-08-21 17:58:37 -0300

    OBI2019-p1: Nota Esquecida

---
+++ obis/fase2/2019/nota_esquecida.cpp
@@ -0,0 +1,11 @@
+// https://olimpiada.ic.unicamp.br/pratique/p1/2019/f2/nota/
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int a, m; cin >> a >> m;
+    cout << m-(a-m);
+
+    return 0;
+}
