9471929f09a4fff7dfc7993f001babe756d8612b
gustavo <83130781+gu-nog@users.noreply.github.com>
2025-11-15 16:13:49 -0300

    Fix sol 1 to diagonal
    Diagonal: obi 2025 f3 p1

--- NOIC/diagonal_sol1.cpp
+++ NOIC/diagonal_sol1.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+pode considerar diagonal mais baixa se fileira atual é última da resposta -> é a i+1
+compara diagonal máxima (maior ou igual)
+*/
+
 const int MAXN = 3e5;
 
-int a[MAXN], allDiags[2*MAXN], freq[MAXN];
+int a[MAXN], allDiags[2*MAXN], freq[2*MAXN];
 map<int, int> mpDiag;
 
 int main() {
@@ -20,13 +25,14 @@ int main() {
 
     int ans = 0, tmp = 0, pt = 0;
     for (int i=0; i<n; i++) {
-        if (a[i] == 0) continue;
+        // manter diags máximas >= i+1
         while (pt < mpDiag[i+1])
             tmp -= freq[pt++];
+        if (a[i] == 0) continue;
         ans = max(ans, tmp + 1);
 
-        if (mpDiag[i+a[i]] <= pt) continue;
-        else freq[mpDiag[i + a[i]]]++, tmp++;
+        if (mpDiag[i+a[i]] >= pt)
+            freq[mpDiag[i + a[i]]]++, tmp++;
     }
 
     cout << ans << '\n';
